4410a1d8184fc8c8ae31819bf23357b3bc031e31
nikhil185182 <119924090+nikhil185182@users.noreply.github.com>
2023-03-01 13:25:03 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OrderTrend from './containers/orderTrend/OrderTrend';
 import { useAppDispatch } from './redux/selectors/hooks';
-import { fetchOrderTrenData, setOrderTrendData } from './redux/reducers/orderTrendReducer';
+import { fetchOrderTrenData} from './redux/reducers/orderTrendReducer';
 
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <BrowserRouter><App/></BrowserRouter>
+        <BrowserRouter><App /></BrowserRouter>
       </Provider>
     </ApolloProvider>
   </React.StrictMode>
