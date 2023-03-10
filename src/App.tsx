857d97673bcf0795d58e4f2c7df6fbc2de59d35b
nikhil185182 <119924090+nikhil185182@users.noreply.github.com>
2023-03-10 10:49:30 +0530

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import { fetchOrderTrenData } from './shared/utils/redux/reducers/orderTrendRedu
 import Dummy from './components/Dummy';
 import NewUserDemo from './containers/newCustomers/newUserDemo';
 import { Button } from '@mui/material';
+import InactiveUsers from './components/InactiveUsers/InactiveUsers';
+import CompanyTrend from './containers/companyLevel';
+import { getInactiveUsersData } from './shared/dto/InactiveUsersDTO';
+import { DataFromGraphql_inactive, DataFromGraphqlUser } from './shared/utils/graphql/gqlHelper';
+import { addingInactiveUsersdata30, addingInactiveUsersdata60, addingInactiveUsersdata90, addingInactiveUsersdata120, addingInactiveUsersdata } from './shared/utils/redux/reducers/InactiveUserReducer';
+import { Fetchnewusersdata } from './shared/utils/redux/reducers/newUserReducer';
 
 
 
@@ -14,6 +20,19 @@ function App() {
   const dispatch = useAppDispatch();
 
   dispatch(fetchOrderTrenData());
+  dispatch(Fetchnewusersdata());
+
+
+  let displaydata30: getInactiveUsersData[] = DataFromGraphql_inactive(30);
+  dispatch(addingInactiveUsersdata30(displaydata30));
+  let displaydata60: getInactiveUsersData[] = DataFromGraphql_inactive(60);
+  dispatch(addingInactiveUsersdata60(displaydata60));
+  let displaydata90: getInactiveUsersData[] = DataFromGraphql_inactive(90);
+  dispatch(addingInactiveUsersdata90(displaydata90));
+  let displaydata120: getInactiveUsersData[] = DataFromGraphql_inactive(120);
+  dispatch(addingInactiveUsersdata120(displaydata120));
+  let displaydata: getInactiveUsersData[] = DataFromGraphqlUser();
+  dispatch(addingInactiveUsersdata(displaydata));
 
   return (
     <>
@@ -23,6 +42,8 @@ function App() {
           <Routes>
             <Route path="/ordertrend" element={<OrderTrend />} />
             <Route path="/newUsers" element={<NewUserDemo />} />
+            <Route path="/companylevel" element={<CompanyTrend  />} />\
+            <Route path="/inactiveUsers" element={<InactiveUsers />} />
           </Routes>
         </div>
       </BrowserRouter>
