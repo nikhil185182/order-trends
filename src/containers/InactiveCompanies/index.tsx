69fd4c75eed25e044d99e222ba4347d877bea0a7
kd185171 <kd185171@ncr.com>
2023-04-13 16:00:38 +0530

    Responsive commit

---
+++ src/containers/InactiveCompanies/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import DateandDaysSelector from '../InactiveDateSelector';
+import InactiveTable from '../InactiveTable';
+import { Datepicker_component, InactiveUsertable_container, Total_component } from './styledComponents';
+import InactiveGraph from '../InactiveGraph';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { useAppDispatch } from '../../shared/utils/redux/hooks';
+
+const InactiveUsers = () => {
+
+  const dispatch = useAppDispatch();
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabSelect = (event: React.SyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
+  };
+
+  return (
+    <Total_component style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={activeTab} onChange={handleTabSelect} aria-label="basic tabs example">
+          <Tab label="MONTHLY DATA" />
+          <Tab label="CONSOLIDATED DATA" />
+        </Tabs>
+      </Box>
+      {activeTab === 1 && (
+        <>
+          <Datepicker_component>
+            <DateandDaysSelector />
+          </Datepicker_component>
+          <InactiveUsertable_container>
+            <InactiveTable />
+          </InactiveUsertable_container>
+        </>
+      )}
+      {activeTab === 0 && <InactiveGraph />}
+    </Total_component>
+  );
+};
+
+
+
+export default InactiveUsers;
+
